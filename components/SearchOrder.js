c44ed75de31b6f868efdce4c823f0d27f3e2a5c4
Martin Pegoraro <martinpegoraro8@gmail.com>
2023-06-23 20:23:18 -0300

    view client order 1.1

--- components/SearchOrder.js
+++ components/SearchOrder.js
@@ -1,5 +1,7 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, SvgIcon, Chip } from '@mui/material'
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import LensIcon from '@mui/icons-material/Lens';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import React from 'react'
 
 const pedidos = {
@@ -36,6 +38,59 @@ export default function SearchOrder() {
                         <LocalShippingIcon className='icon-icon' sx={{}} />
                     </Grid>
                 </Grid>
+
+                <Grid container justifyContent="center" alignItems="center" sx={{ width: '90%', ml: '5%' }}>
+                    <Grid item xs={0.5}>
+                        <div class="circuloInicio"></div>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <div class="linea-horizontalInicio"></div>
+                    </Grid>
+                    <Grid item xs={0.5}>
+                        {pedidos.lugar.length > 2 ?
+                            <div class="circuloInicio"></div>
+                            :
+                            <div class="circulo"></div>
+                        }
+                    </Grid>
+                    <Grid item xs={1}>
+                        {pedidos.lugar.length > 2 ?
+                            <div class="linea-horizontalInicio"></div>
+                            :
+                            <div class="linea-horizontal"></div>
+                        }
+                    </Grid>
+                    <Grid item xs={0.5}>
+                        {pedidos.entregado ?
+                            <div class="circuloInicio"></div>
+                            :
+                            <div class="circulo"></div>
+                        }
+                    </Grid>
+                </Grid>
+
+
+                <Grid container justifyContent="center" alignItems="center" sx={{ width: '90%', ml: '5%' }}>
+                    <Grid item xs={0.5}>
+                        <p>inicio</p>
+
+                    </Grid>
+                    <Grid item xs={1}>
+                    </Grid>
+                    <Grid item xs={0.5}>
+                        <p>viajando</p>
+                    </Grid>
+                    <Grid item xs={1.2}>
+
+                    </Grid>
+                    <Grid item xs={0.5}>
+                        <p>llego</p>
+                    </Grid>
+                </Grid>
+
+
+
+
                 <Box sx={{ mt: 2, pb: 2, width: '90%', minHeight: 300, borderRadius: 2, ml: '5%' }}>
                     <Typography variant="subtitle1" sx={{ p: 1, bgcolor: '#1976d2', borderTopLeftRadius: 6.5, borderTopRightRadius: 6.5 }}>Este es el resultado de la consulta:
                         <Typography variant="span" sx={{ bgcolor: 'rgb(0, 191, 255)', borderRadius: 1, ml: 0.5 }}> {pedidos.nroPedido}</Typography>
